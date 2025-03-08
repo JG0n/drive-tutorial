d05ba4e8d02ffd732871b7588a37de21a11585a9
JG0n <EduardoGoncalves98@protonmail.com>
2025-03-08 22:41:14 +0000

    home page and onboarding

--- src/server/db/queries.ts
+++ src/server/db/queries.ts
@@ -1,6 +1,6 @@
 import "server-only";
 
-import { eq } from "drizzle-orm";
+import { and, eq, isNull } from "drizzle-orm";
 import { db } from "~/server/db";
 import {
 	files_table as filesSchema,
@@ -49,6 +49,19 @@ export const QUERIES = {
 			.where(eq(foldersSchema.id, folderId));
 		return folder[0];
 	},
+
+	getRootFolderForUser: async function (userId: string) {
+		const folder = await db
+			.select()
+			.from(foldersSchema)
+			.where(
+				and(
+					eq(foldersSchema.ownerId, userId),
+					isNull(foldersSchema.parent),
+				),
+			);
+		return folder[0];
+	},
 };
 
 export const MUTATIONS = {
@@ -64,5 +77,38 @@ export const MUTATIONS = {
 		return await db
 			.insert(filesSchema)
 			.values({ ...input.file, ownerId: input.userId });
+	},
+
+	onboardUser: async function (userId: string) {
+		const rootFolder = await db
+			.insert(foldersSchema)
+			.values({
+				name: "Root",
+				parent: null,
+				ownerId: userId,
+			})
+			.$returningId();
+
+		const rootFolderId = rootFolder[0]!.id;
+
+		await db.insert(foldersSchema).values([
+			{
+				name: "Trash",
+				parent: rootFolderId,
+				ownerId: userId,
+			},
+			{
+				name: "Shared",
+				parent: rootFolderId,
+				ownerId: userId,
+			},
+			{
+				name: "Documents",
+				parent: rootFolderId,
+				ownerId: userId,
+			},
+		]);
+
+		return rootFolderId;
 	},
 };
